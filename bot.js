f03771fa81fdc6948cd6314fbff2ce694513df45
fathisye <47281515+fathisye@users.noreply.github.com>
2019-03-20 01:51:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -229,8 +229,8 @@ function play(guild, song) {
     serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }
  
-const adminprefix = "$vip";
-const devs = ['274923685985386496'];
+const adminprefix = "$";
+const devs = ['511234899655786503'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
